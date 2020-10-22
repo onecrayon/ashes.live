132f725e7b17b1dfad6a5608b08d3a793ca953a7
Ian Beck <ian@onecrayon.com>
2020-10-22 00:01:58 -0700

    Got modular CSS working (have to reference the style programmatically for local development to work)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <nav class="bg-inexhaustible">
-    <div class="banner">
+    <!-- Referencing the style directly through an injected $style element is necessary for modular styles to function; otherwise, the template style name doesn't get rewritten -->
+    <div :class="$style.banner">
       <ul class="grid items-center gap-2 grid-cols-3 grid-rows-2 md:grid-rows-1 lg:container lg:mx-auto text-center py-2 px-4 lg:py-4">
         <li class="row-start-1 col-start-1 col-span-3 md:col-start-2 md:col-span-1">
-          <a href="/" class="home-link">Ashes.live</a>
+          <a href="/" :class="$style['home-link']">Ashes.live</a>
         </li>
         <li class="row-start-1 col-start-1 row-start-2 md:row-start-1 text-gray">(upcoming)</li>
         <li class="row-start-1 col-start-3 row-start-2 md:row-start-1 text-gray">(upcoming)</li>
@@ -25,8 +26,8 @@ export default {
 }
 </script>
 
-<!-- TODO: add `module` to this definition when it actually works with Vite! -->
-<style lang="postcss">
+<!-- Specifying the lang as "postcss" isn't strictly necessary, but makes VS Code syntax highlighting work properly -->
+<style lang="postcss" module>
 .banner {
   position: relative;
 
