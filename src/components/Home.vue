767454c1a05cadaaa09f2d7542d1757ec90aeed5
Ian Beck <ian@onecrayon.com>
2020-10-23 22:40:34 +0000

    Removed extraneous wrapper element from homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,9 @@
 <template>
-  <article class="p-4 lg:container lg:mx-auto lg:pt-8">
-    <h1 class="phg-time-class">Work in progress</h1>
+  <h1 class="phg-time-class">Work in progress</h1>
 
-    <p>Thank you for your interest in the next generation Ashes.live site! I am currently hard at work building out an even better experience for Ashes Reborn.</p>
+  <p>Thank you for your interest in the next generation Ashes.live site! I am currently hard at work building out an even better experience for Ashes Reborn.</p>
 
-    <p>If you are interested in contributing, please <a href="https://github.com/onecrayon/ashes.live">visit the GitHub repo</a>.</p>
-  </article>
+  <p>If you are interested in contributing, please <a href="https://github.com/onecrayon/ashes.live">visit the GitHub repo</a>.</p>
 </template>
 
 <script>
