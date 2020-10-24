69acef42c9ab8fad3b499d263dbbe91d2f12dbfc
Ian Beck <ian@onecrayon.com>
2020-10-24 00:26:54 +0000

    WIP: Added main navigation links
    Currently they don’t do anything, but the responsive behavior is working properly.

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,26 @@
   <nav class="bg-inexhaustible" :class="[useFullHeader ? $style['full-header'] : '']">
     <!-- Referencing the style directly through an injected $style element is necessary for modular styles to function; otherwise, the template style name doesn't get rewritten -->
     <div :class="$style.banner">
-      <ul class="grid items-center gap-2 grid-cols-3 grid-rows-2 md:grid-rows-1 lg:container lg:mx-auto text-center py-2 px-4 lg:py-4">
-        <li class="row-start-1 col-start-1 col-span-3 md:col-start-2 md:col-span-1">
+      <ul class="container mx-auto grid items-center text-center py-2 px-4 gap-x-10 grid-rows-2 md:grid-rows-1 lg:py-4" :class="$style['header-cols']">
+        <li class="col-start-1 col-span-3 row-start-1 md:col-start-2 md:col-span-1">
           <router-link to="/" :class="$style['home-link']">Ashes.live</router-link>
         </li>
-        <li class="row-start-1 col-start-1 row-start-2 md:row-start-1 text-gray">(upcoming)</li>
-        <li class="row-start-1 col-start-3 row-start-2 md:row-start-1 text-gray">(upcoming)</li>
+        <li class="col-start-1 row-start-2 justify-self-start md:row-start-1 lg:justify-self-end">
+          <router-link to="/decks" class="inline-block text-black leading-tight">
+            <i class="phg-main-action phg-standalone text-2xl pr-2 inline-block lg:block lg:pr-0"></i>
+            <span class="text-lg">Decks</span>
+          </router-link>
+        </li>
+        <li class="col-start-3 row-start-2 justify-self-end md:row-start-1 lg:justify-self-start">
+          <router-link to="/cards" class="inline-block text-black leading-tight">
+            <i class="phg-side-action phg-standalone text-2xl pr-2 inline-block lg:block lg:pr-0"></i>
+            <span class="text-lg">Cards</span>
+          </router-link>
+        </li>
       </ul>
     </div>
   </nav>
-  <div class="p-4 lg:container lg:mx-auto lg:pt-8">
+  <div class="p-4 container mx-auto lg:pt-8">
     <router-view></router-view>
   </div>
 </template>
@@ -45,6 +55,10 @@ export default {
   }
 }
 
+.header-cols {
+  grid-template-columns: minmax(0, 1fr) minmax(0, 1fr) minmax(0, 1fr);
+}
+
 .home-link {
   background: url('./assets/ashes-live-small.png') left top no-repeat;
   display: inline-block;
@@ -59,10 +73,15 @@ export default {
     background-image: url('./assets/ashes-live.png');
     width: 366px;
     padding-top: 107px;
+    margin-top: 8px;
     /* Adjust it downward so that we overlap the border; this equates to 45px at base 16 */
     margin-bottom: -2.8125rem;
     position: relative;
     z-index: 10;
+  }
+
+  .header-cols {
+    grid-template-columns: minmax(0, 1fr) 366px minmax(0, 1fr);
   }
 }
 </style>
