893127cfceab2408a3c73b1a297733f856bb90e0
Ian Beck <ian@onecrayon.com>
2020-10-25 19:21:28 -0700

    WIP: Implemented basic filter text tracking for the text filter

--- src/components/shared/card_browser/CardBrowser.vue
+++ src/components/shared/card_browser/CardBrowser.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="sm:flex sm:flex-no-wrap">
       <dice-filter class="flex-none mb-4 h-10 sm:pr-4 md:mb-0"></dice-filter>
-      <text-filter class="flex-auto h-10"></text-filter>
+      <text-filter @text-filter="filterByText" class="flex-auto h-10"></text-filter>
     </div>
     <!-- TODO: implement the rest of the filters -->
     <card-listing :is-phoenixborn-picker="isPhoenixbornPicker"></card-listing>
@@ -23,7 +23,12 @@ export default {
     DiceFilter,
     TextFilter,
     CardListing,
-    PhoenixbornFilters,
-  }
+  },
+  methods: {
+    filterByText (value) {
+      // TODO: filter by the given text value
+      console.log('Filter with text: ', value)
+    },
+  },
 }
 </script>
--- src/components/shared/card_browser/TextFilter.vue
+++ src/components/shared/card_browser/TextFilter.vue
@@ -2,12 +2,36 @@
   <div class="flex flex-no-wrap">
     <input class="appearance-none border-2 border-gray-darker border-r rounded-l-md h-full px-2 flex-auto"
       type="text" placeholder="Filter by name or text..."
-    ><button class="btn btn-last h-full px-4 text-l flex-none" title="Clear Search"><i class="fas fa-times"></i></button>
+      v-model="filter"
+    ><button class="btn btn-last h-full px-4 text-l flex-none" title="Clear Search"
+      @click="filter = null">
+      <i class="fas fa-times"></i>
+    </button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TextFilter',
+  emits: {
+    'text-filter': null,
+  },
+  data: () => {
+    return {
+      filterText: '',
+    }
+  },
+  computed: {
+    filter: {
+      get () {
+        return this.filterText
+      },
+      set (value) {
+        this.filterText = value || ''
+        // Pass the filter value up to the parent component
+        this.$emit('text-filter', event.target.value)
+      },
+    }
+  },
 }
 </script>
