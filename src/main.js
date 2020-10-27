216bc3bd93d133f484146ec880fa6889f10b58cc
Ian Beck <ian@onecrayon.com>
2020-10-26 22:51:40 -0700

    Added Noty for ephemeral site notifications

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import router from './router.js'
 import App from './App.vue'
 import './index.css'
+import 'noty/lib/noty.css'
+import 'noty/lib/themes/metroui.css'
 
 const vue = createApp(App)
 vue.use(router)
