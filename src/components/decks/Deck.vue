57504b4970a15fdbb1fc29f08dc72d6194d9ae31
Fabien Deshayes <fabien.deshayes@gmail.com>
2020-12-24 15:58:44 +0000

    Add fields to Deck in deck list

--- src/components/decks/Deck.vue
+++ src/components/decks/Deck.vue
@@ -1,18 +1,29 @@
 <template>
   <div
-    class="border border-gray bg-white"
-    :class="[$style.deck]">
-    <div class="clearfix">
-      <div class="px-2 py-px text-xs">
-        <p class="m-0 font-bold text-lg">
-          <router-link :to="linkTarget" class="text-black">{{ deck.title }}</router-link>
-        </p>
-      </div>
+    class="border border-gray bg-white mt-4 mb-4">
+    <div class="px-2 py-px text-xs">
+      <p class="m-0 font-bold text-lg">
+        <router-link :to="linkTarget" class="text-black">{{ deck.title }}</router-link>
+      </p>
+      <span class="text-sm">
+        {{ deck.user.username }}
+      </span>
+      <span class="text-sm float-right">
+        Last updated: {{ lastUpdatedDateFormatted }} ago
+      </span>
+      <hr />
+      <span class="text-lg">
+        {{ deck.phoenixborn.name }}
+      </span>
+      <span class="text-sm float-right">
+        {{ cardsCount }}/30
+      </span>
     </div>
   </div>
 </template>
 
 <script>
+import { parseISO, formatDistanceToNowStrict } from 'date-fns'
 import { typeToFontAwesome } from '/src/constants.js'
 
 export default {
@@ -39,14 +50,16 @@ export default {
         params: { id: this.deck.id },
       }
     },
+    lastUpdatedDateFormatted () {
+      return formatDistanceToNowStrict(parseISO(this.deck.modified))
+    },
+    cardsCount () {
+      return this.deck.cards.reduce((prev, card) => {
+        return prev + card.count
+      }, 0)
+    }
   },
   methods: {
   },
 }
 </script>
-
-<style lang="postcss" module>
-.card {
-  width: 300px;
-}
-</style>
