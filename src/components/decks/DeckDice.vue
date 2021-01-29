13cb369c72d874681cf82a902fa82757c6111469
Ian Beck <ian@onecrayon.com>
2021-01-28 22:28:02 -0800

    Scoped DeckDice styles to the module

--- src/components/decks/DeckDice.vue
+++ src/components/decks/DeckDice.vue
@@ -1,7 +1,10 @@
 <template>
-  <ul class="dice">
+  <ul :class="$style.dice">
     <template v-for="(die, idx) of dice" :key="idx">
-      <li v-for="index in die.count" :key="index" :class="['phg-' + die.name + '-power', die.name, 'die']" class="w-6 h-6 text-sm sm:w-8 sm:h-8 sm:text-xl">
+      <li
+        v-for="index in die.count" :key="index"
+        class="w-6 h-6 text-sm sm:w-8 sm:h-8 sm:text-xl die"
+        :class="['phg-' + die.name + '-power', die.name, $style.die]">
       </li>
     </template>
   </ul>
@@ -14,7 +17,7 @@ export default {
 }
 </script>
 
-<style>
+<style module>
 .dice {
   display: flex;
   margin: 0;
@@ -28,40 +31,5 @@ export default {
 }
 .die:before {
   padding: 0;
-}
-.ceremonial {
-  color: #ee3342;
-  background: #111821;
-  border-color: #ee3342;
-}
-.charm {
-  color: #f7e442;
-  background: #f34f98;
-  border-color: #f7e442;
-}
-.illusion {
-  color: #8bc7ea;
-  background: #490d67;
-  border-color: #8bc7ea;
-}
-.natural {
-  color: #cedb00;
-  background: #004a98;
-  border-color: #cedb00;
-}
-.divine {
-  color: #b58a2b;
-  background: #f2f2f2;
-  border-color: #b58a2b;
-}
-.sympathy {
-  color: #e3c2af;
-  background: #119981;
-  border-color: #e3c2af;
-}
-.time {
-  color: #dfdfdf;
-  background: #9a2e36;
-  border-color: #dfdfdf;
 }
 </style>
