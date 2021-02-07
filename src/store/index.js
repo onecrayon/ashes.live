0362cc94f1bba4130ee9248b1c5490d87d550cd1
Ian Beck <ian@onecrayon.com>
2021-02-07 10:17:39 -0800

    WIP: Added very basic deck store skeleton

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore, createLogger } from 'vuex'
 import cards from './cards.js'
+import deck from './deck.js'
 import player from './player.js'
 import options from './options.js'
 
@@ -8,6 +9,7 @@ const debug = process.env.NODE_ENV !== 'production'
 export default createStore({
   modules: {
     cards,
+    deck,
     player,
     options,
   },
