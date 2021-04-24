aafef0a5894e5c3ca853ea5660129eed71876b6a
nminchow <noelminchow@gmail.com>
2021-04-24 17:56:46 -0400

    seperate summons toggle

---
+++ src/components/cards/card_browser/FilterText.vue
@@ -0,0 +1,22 @@
+<template>
+  <i :class="typeIcon(type)"></i> <span class="hidden xl:inline">{{ text || type.split(' ')[0] }}</span>
+</template>
+
+<script>
+import { typeToFontAwesome } from '/src/constants.js'
+
+export default {
+  name: 'FilterIcon',
+  props: {
+    type: String,
+    text: String,
+  },
+  methods: {
+    typeIcon (targetType) {
+      if (targetType === 'Summon') return 'fas fa-plus-square'
+      const typeClass = typeToFontAwesome[targetType]
+      return typeClass ? typeClass : 'fa-question-circle'
+    },
+  }
+}
+</script>
