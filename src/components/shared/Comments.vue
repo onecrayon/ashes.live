7fb0b35ba9da1d1263e3ae52b69385e3731a9e77
Ian Beck <ian@onecrayon.com>
2024-06-10 20:15:21 -0700

    Updated comment form to maintain the Publish button when previewing the comment
    Also tweaked behavior to avoid full page refreshes upon comment submission.

--- src/components/shared/Comments.vue
+++ src/components/shared/Comments.vue
@@ -69,27 +69,25 @@
       </div>
 
       <!-- TODO: update this to use the standard editing form controls -->
-      <div v-if="commentWriteMode">
-        <form @submit.prevent="submitComment" class="flex flex-col">
-          <text-editor
-            placeholder="Enter comment here"
-            v-model="commentText"
-          >
-          </text-editor>
-          <div class="mt-2 md:flex">
-            <p class="text-gray text-sm mb-4 mt-0 md:flex-1 md:order-2 md:pt-2 md:text-right">
-              Please respect the <router-link to="/policies/">Ashes.live Content Policies</router-link>.
-            </p>
-            <button class="btn btn-blue py-1 mb-4 w-full md:flex-none md:px-4 md:w-auto" :disabled="!commentText">Publish</button>
-          </div>
-        </form>
-      </div>
-      <div v-else>
-        <card-codes
-          :content="commentText"
-          class="px-2 py-1 m-0 border-2 rounded px-4 py-2 border-inexhaustible-dark bg-inexhaustible"
-          needs-paragraphs></card-codes>
-      </div>
+      <form @submit.prevent="submitComment" class="flex flex-col">
+        <text-editor
+          placeholder="Enter comment here"
+          v-model="commentText"
+          v-show="commentWriteMode"
+        ></text-editor>
+        <div v-if="!commentWriteMode">
+          <card-codes
+            :content="commentText"
+            class="px-2 py-1 m-0 border-2 rounded px-4 py-2 border-inexhaustible-dark bg-inexhaustible"
+            needs-paragraphs></card-codes>
+        </div>
+        <div class="mt-2 md:flex">
+          <p class="text-gray text-sm mb-4 mt-0 md:flex-1 md:order-2 md:pt-2 md:text-right">
+            Please respect the <router-link to="/policies/">Ashes.live Content Policies</router-link>.
+          </p>
+          <button class="btn btn-blue py-1 mb-4 w-full md:flex-none md:px-4 md:w-auto" :disabled="!commentText">Publish</button>
+        </div>
+      </form>
     </div>
     <div v-else>
       <button class="btn btn-blue px-4 py-1 mb-4" @click="triggerLogin">Log in to comment</button>
@@ -139,34 +137,37 @@ export default {
     TextEditor,
   },
   beforeMount () {
-    request(`/v2/comments/${this.entityId}`).then(response => {
-      this.comments = response.data.results
-      this.loading = false
-    }).catch(error => {
-      this.error = true
-      this.loading = false
-    })
+    this.refreshComments()
   },
   computed: {
     isAuthenticated () {
       return this.$store.getters['player/isAuthenticated']
     },
-    submitComment () {
-      request(`v2/comments/${this.entityId}`, {
-        method: 'post',
-        data: {text: this.commentText }
-      }).then(() => {
-        this.$router.go()  // Refresh current page
-      }).catch((error) => {
-        this.handleResponseError(error)
-        this.error = true
-      })
-    },
   },
   methods: {
+    refreshComments () {
+      this.loading = true
+      request(`/v2/comments/${this.entityId}`).then(response => {
+        this.comments = response.data.results
+        this.loading = false
+      }).catch(error => {
+        this.error = true
+        this.loading = false
+      })
+    },
     triggerLogin () {
       emitter.emit("login:required")
-    }
+    },
+    submitComment () {
+      request(`/v2/comments/${this.entityId}`, {
+        method: 'post',
+        data: { text: this.commentText }
+      }).then(() => {
+        this.refreshComments()
+      }).catch((error) => {
+        this.handleResponseError(error)
+      })
+    },
   }
 }
 </script>
