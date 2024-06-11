f1e40ccd67cd8d7d727bf490966b69c6fab7182a
Ian Beck <ian@onecrayon.com>
2024-06-10 17:05:17 -0700

    Added formatting help modal
    Also fixed regex logic for detecting blockquotes (missing "dot matches newline" and also was failing to catch quotes where the only text was in a quote), and fixed some display issues with the main comment form.

--- src/components/shared/Comments.vue
+++ src/components/shared/Comments.vue
@@ -35,7 +35,7 @@
     <div v-if="isAuthenticated && !loading && !error">
       <h3>Post a comment</h3>
 
-      <div class="flex flex-nowrap mb-4">
+      <div class="flex flex-nowrap mb-1">
         <button class="btn btn-first"
           :class="{active: commentWriteMode}"
           @click="commentWriteMode = true">
@@ -47,13 +47,28 @@
           @click="commentWriteMode = false">
           Preview
         </button>
-        <link-alike class="ml-2 py-1" use-color use-underline>
+        <link-alike class="ml-2 py-1" use-color use-underline @click="formattingHelpOpen = !formattingHelpOpen">
           <i class="fas fa-question-circle"></i>
           Formatting help
         </link-alike>
+        <modal v-model:open="formattingHelpOpen">
+          <div class="max-w-xl mx-auto">
+            <h2>Comment formatting</h2>
+
+            <card-code-example :text="'**Bold text**'"></card-code-example>
+            <card-code-example :text="'*Italic text*'"></card-code-example>
+            <card-code-example :text="'[[main]] - 1 [[charm:class]]'"></card-code-example>
+            <card-code-example :text="'[[Hammer Knight]]'"></card-code-example>
+            <card-code-example :text="'[[Link https://ashteki.com]]'"></card-code-example>
+            <card-code-example :text="'[[*https://cdn.ashes.live/images/slices/channel-magic.jpg]]'"></card-code-example>
+            <card-code-example :text="'* Unordered item 1\n* Unordered item 2'"></card-code-example>
+            <card-code-example :text="'~ Ordered item 1\n~ Ordered item 2'"></card-code-example>
+            <card-code-example :text="'> Quoted text'"></card-code-example>
+          </div>
+        </modal>
       </div>
 
-      <!-- TODO: update this to use the standard editing form controls; add old-style card code stuff? Inline help modal? -->
+      <!-- TODO: update this to use the standard editing form controls -->
       <div v-if="commentWriteMode">
         <form @submit.prevent="submitComment" class="flex flex-col">
           <text-editor
@@ -61,8 +76,12 @@
             v-model="commentText"
           >
           </text-editor>
-          <p class="text-gray text-sm mb-4 mt-1">Please respect the <router-link to="/policies/">Ashes.live Content Policies</router-link>.</p>
-          <button class="btn btn-blue px-2 py-1 mb-4">Publish</button>
+          <div class="mt-2 md:flex">
+            <p class="text-gray text-sm mb-4 mt-0 md:flex-1 md:order-2 md:pt-2 md:text-right">
+              Please respect the <router-link to="/policies/">Ashes.live Content Policies</router-link>.
+            </p>
+            <button class="btn btn-blue py-1 mb-4 w-full md:flex-none md:px-4 md:w-auto" :disabled="!commentText">Publish</button>
+          </div>
         </form>
       </div>
       <div v-else>
@@ -84,7 +103,9 @@ import { getRelativeDateString } from '/src/utils/dates.js'
 import { request } from '/src/utils/index.js'
 import emitter from '/src/events.js'
 import CardCodes from './CardCodes.vue'
+import CardCodeExample from './CardCodeExample.vue'
 import LinkAlike from './LinkAlike.vue'
+import Modal from './Modal.vue'
 import TextEditor from './TextEditor.vue'
 import PlayerBadge from './PlayerBadge.vue'
 
@@ -97,6 +118,7 @@ export default {
     commentText: '',
     error: false,
     commentWriteMode: true,
+    formattingHelpOpen: false,
   }),
   setup () {
     const formatCommentDate = (timestamp) => {
@@ -110,7 +132,9 @@ export default {
   },
   components: {
     CardCodes,
+    CardCodeExample,
     LinkAlike,
+    Modal,
     PlayerBadge,
     TextEditor,
   },
